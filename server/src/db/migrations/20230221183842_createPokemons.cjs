146b4e5c0f946ce87c3c711d16010973af436c27
Xun <xun@Xuns-MacBook-Pro.local>
2023-02-21 15:44:57 -0500

    fixed typo in Pokemon migration table

--- server/src/db/migrations/20230221183842_createPokemons.cjs
+++ server/src/db/migrations/20230221183842_createPokemons.cjs
@@ -7,7 +7,7 @@
  */
 exports.up = async (knex) => {
   return knex.schema.createTable("pokemons", (table) => {
-    table.bigInrecments("id")
+    table.bigIncrements("id")
     table.string("name").notNullable()
     table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
     table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
