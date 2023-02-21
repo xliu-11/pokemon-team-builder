2b90293c178cd9499b0cbc3bff21f885a3b61fc7
Xun <xun@Xuns-MacBook-Pro.local>
2023-02-21 13:56:53 -0500

    created migration tables for pokemon and userPokemon

---
+++ server/src/db/migrations/20230221184319_createUserPokemon.cjs
@@ -0,0 +1,31 @@
+/**
+ * @typedef {import("knex")} Knex
+ */
+
+/**
+ * @param {Knex} knex
+ */
+exports.up = async (knex) => {
+  return knex.schema.createTable("userPokemons", (table) => {
+    table.bigIncrements("id")
+    table.bigInteger("userId")
+         .unsigned()
+         .notNullable()
+         .index()
+         .references("users.id")
+    table.bigInteger("pokemonId")
+         .unsigned()
+         .notNullable()
+         .index()
+         .references("pokemons.id")    
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
+    table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
+  })
+}
+
+/**
+ * @param {Knex} knex
+ */
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists("usersPokemons")
+}
