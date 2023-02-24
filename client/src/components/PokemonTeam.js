444da9d93f6d6aad37331a3fe5f7958bd5d94ada
Xun <xun@Xuns-MacBook-Pro.local>
2023-02-24 18:27:14 -0500

    added delete feature on team page

--- client/src/components/PokemonTeam.js
+++ client/src/components/PokemonTeam.js
@@ -19,74 +19,110 @@ const PokemonTeam = (props) => {
     }
   };
 
+  const deletePokemon = async (id) => {
+    try {
+      const response = await fetch(`/api/v1/teams/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`;
+        const error = new Error(errorMessage);
+        throw error;
+      }
+      setTeam(prevTeam => prevTeam.filter(pokemon => pokemon.id !== id));
+    } catch (error) {
+      console.error(`Error in fetch: ${error.message}`);
+    }
+  };  
+
   useEffect(() => {
-    fetchTeamData();
-  }, []);
+    let isMounted = true;
+    fetchTeamData().then(() => {
+      if (isMounted) {
+        // Only update state if component is still mounted
+        setTeam(prevTeam => prevTeam.slice(0, 6));
+      }
+    });
+    return () => {
+      isMounted = false;
+    }
+  }, [team]);
+
+  const topRow = team.slice(0, 3);
+  const bottomRow = team.slice(3, 6);
 
   return (
     <div className="text-center">
       <h2>My Pokémon Team</h2>
       <div className="pokemon-row">
-        {team.slice(0, 3).map((pokemon) => (
-          <div className="pokemon-info-col" key={pokemon.id}>
+        {topRow.map((pokemon) => (
+          <div className="pokemon-container" key={pokemon.id}>
             <h4>{pokemon.name}</h4>
             <img src={pokemon.image} alt={pokemon.name} />
             <p>
-          <strong>Type: </strong> {pokemon.type}
-          {pokemon.secondaryType && `, ${pokemon.secondaryType}`}
-        </p>
-        <p>
-          <strong>Abilities:</strong> {pokemon.abilities.join(", ")}
-          {pokemon.hiddenAbility && (
-            <>
-              <br />
-                <strong>Hidden Ability:</strong> {pokemon.hiddenAbility}
-            </>
-          )}
-        </p>
-        <h6><strong>Stats:</strong></h6>
-        <ul>
-          {pokemon.stats.map((stat) => (
-            <p key={stat.name}>
-              <strong>{stat.name}:</strong> {stat.value}
+              <strong>Type: </strong> {pokemon.type}
+              {pokemon.secondaryType && `, ${pokemon.secondaryType}`}
             </p>
-          ))}
-        </ul>
+            <p>
+              <strong>Abilities:</strong> {pokemon.abilities.join(", ")}
+              {pokemon.hiddenAbility && (
+                <>
+                  <br />
+                  <strong>Hidden Ability:</strong> {pokemon.hiddenAbility}
+                </>
+              )}
+            </p>
+            <h6>
+              <strong>Stats:</strong>
+            </h6>
+            <ul>
+              {pokemon.stats.map((stat) => (
+                <p key={stat.name}>
+                  <strong>{stat.name}:</strong> {stat.value}
+                </p>
+              ))}
+            </ul>
+            <button onClick={() => deletePokemon(pokemon.id)}>Delete</button>
           </div>
         ))}
       </div>
-      <div className="lower-row">
-        {team.slice(3, 6).map((pokemon) => (
-          <div className="pokemon-info-col" key={pokemon.id}>
-          <h4>{pokemon.name}</h4>
-          <img src={pokemon.image} alt={pokemon.name} />
-          <p>
-        <strong>Type: </strong> {pokemon.type}
-        {pokemon.secondaryType && `, ${pokemon.secondaryType}`}
-      </p>
-      <p>
-        <strong>Abilities:</strong> {pokemon.abilities.join(", ")}
-        {pokemon.hiddenAbility && (
-          <>
-            <br />
-              <strong>Hidden Ability:</strong> {pokemon.hiddenAbility}
-          </>
-        )}
-      </p>
-      <h6><strong>Stats:</strong></h6>
-      <ul>
-        {pokemon.stats.map((stat) => (
-          <p key={stat.name}>
-            <strong>{stat.name}:</strong> {stat.value}
-          </p>
-        ))}
-      </ul>
-        </div>
+      <div className="pokemon-row">
+        {bottomRow.map((pokemon) => (
+          <div className="pokemon-container" key={pokemon.id}>
+            <h4>{pokemon.name}</h4>
+            <img src={pokemon.image} alt={pokemon.name} />
+            <p>
+              <strong>Type: </strong> {pokemon.type}
+              {pokemon.secondaryType && `, ${pokemon.secondaryType}`}
+            </p>
+            <p>
+              <strong>Abilities:</strong> {pokemon.abilities.join(", ")}
+              {pokemon.hiddenAbility && (
+                <>
+                  <br />
+                  <strong>Hidden Ability:</strong> {pokemon.hiddenAbility}
+                </>
+              )}
+            </p>
+            <h6>
+              <strong>Stats:</strong>
+            </h6>
+            <ul>
+              {pokemon.stats.map((stat) => (
+                <p key={stat.name}>
+                  <strong>{stat.name}:</strong> {stat.value}
+                </p>
+              ))}
+            </ul>
+            <button onClick={() => deletePokemon(pokemon.id)}>Delete</button>
+          </div>
         ))}
       </div>
-      <Link to="/">Search for Another Pokémon to Add to Your Team!</Link>
     </div>
-  );
+  )
 };
 
 export default PokemonTeam;
