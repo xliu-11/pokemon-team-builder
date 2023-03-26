a2070132749b7c3140187434b03c240364559d1a
Xun <xun@Xuns-MacBook-Pro.local>
2023-03-26 01:24:57 -0400

    moved the fetch request of the featured pokemon section to the backend

--- server/src/routes/api/v1/homepageRouter.js
+++ server/src/routes/api/v1/homepageRouter.js
@@ -26,4 +26,36 @@ homepageRouter.get("/:name", async (req, res) => {
   }
 });
 
+homepageRouter.get("/", async (req, res) => {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1008`);
+    if (!response.ok) {
+      const errorMessage = `${response.status} (${response.statusText})`;
+      const error = new Error(errorMessage);
+      throw error;
+    }
+    const data = await response.json();
+    const randomIndex = Math.floor(Math.random() * data.results.length);
+    const randomPokemonUrl = data.results[randomIndex].url;
+    const randomPokemonResponse = await fetch(randomPokemonUrl);
+    const randomPokemonData = await randomPokemonResponse.json();
+    const featuredPokemon = {
+      image: randomPokemonData.sprites.front_default,
+      name: randomPokemonData.name.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase()),
+      type: randomPokemonData.types[0].type.name.replace(/\b\w/g, (char) => char.toUpperCase()),
+      secondaryType: randomPokemonData.types.length > 1 ? randomPokemonData.types[1].type.name.replace(/\b\w/g, (char) => char.toUpperCase()) : null,
+      ability: randomPokemonData.abilities.map((ability) => ability.ability.name.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase())),
+      stats: randomPokemonData.stats.map((stat) => ({
+        name: stat.stat.name.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase()),
+        value: stat.base_stat,
+      })),
+    };
+    res.json(featuredPokemon);
+  } catch (err) {
+    console.error(`Error in fetch: ${err.message}`);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
 export default homepageRouter;
